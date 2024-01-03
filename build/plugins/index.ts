966a194db0fd3454643a31e6ba9453f1a4e39706
wangyong_wd <wangyong@wondertek.com.cn>
2024-01-03 19:30:37 +0800

    ci(projects): 更新打包配置,支持自动打包成zip

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -5,6 +5,8 @@ import unocss from '@unocss/vite';
 import progress from 'vite-plugin-progress';
 import VueDevtools from 'vite-plugin-vue-devtools';
 import mockDevServerPlugin from 'vite-plugin-mock-dev-server';
+import compresssionBuild from 'rollup-plugin-compression';
+import type { ICompressionOptions } from 'rollup-plugin-compression';
 import unplugin from './unplugin';
 import visualizer from './visualizer';
 import compress from './compress';
@@ -14,7 +16,21 @@ import pwa from './pwa';
  * @param viteEnv - 环境变量配置
  */
 export function setupVitePlugins(viteEnv: ImportMetaEnv): (PluginOption | PluginOption[])[] {
-  const plugins = [VueDevtools(), vue(), vueJsx(), ...unplugin(viteEnv), unocss(), progress()];
+  const option: ICompressionOptions = {
+    sourceName: `editor-template`,
+    type: 'zip',
+    targetName: `editor-template`,
+    ignoreBase: false // 默认打包源文件夹本身,配置为true则只打包文件夹内文件  默认是false,
+  };
+  const plugins = [
+    VueDevtools(),
+    vue(),
+    vueJsx(),
+    ...unplugin(viteEnv),
+    unocss(),
+    progress(),
+    compresssionBuild(option)
+  ];
 
   if (viteEnv.VITE_VISUALIZER === 'Y') {
     plugins.push(visualizer as PluginOption);
